a0319b5b1fc2fc50f1a89ee3d8ec34c4b7fe9b6f
Martha Mwaura <marthamwaurah@gmail.com>
2023-08-11 13:08:46 +0000

    source configuration

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -5,4 +5,4 @@ select
     status,
     amount / 100 as amount, -- amount is stored in cents, convert to dollars
     created as created_at
-from raw.stripe.payment
+from {{source("stripe", 'payment')}}
